8a8e80bf5b83cb11b082961aa8e46ed6a212d1a7
vper <89226782@qq.com>
2017-02-28 09:40:22 +0800

    测试1

--- app.js
+++ app.js
@@ -1,6 +1,51 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World o my god tangren\n');
-}).listen(9001);
-console.log('Server running at http://127.0.0.1:9001/');
+var exec = require('child_process').exec;
+console.log("################################");
+const PORT = 7001
+  , PATH = '/usr/jenkins/workspace/test'
+
+var deployServer = http.createServer(function(request, response) {
+  if (request.url.search(/deploy/i) > 0) {
+	
+    var commands = [
+      'cd ' + PATH,
+	  //'git fetch --all',
+	  //'git reset --hard origin/master',
+      //'git pull '
+    ].join(' && ')
+
+    exec(commands, function(err, out, code) {
+      if (err instanceof Error) {
+        response.writeHead(500)
+        response.end('Server Internal Error.')
+        throw err
+      }
+      process.stdout.write(out);
+      response.writeHead(200);
+      response.end('Deploy Done.'+ showLeftTime())
+	console.log("更新完成！ ",request.url);
+    })
+
+  } else {
+
+    response.writeHead(404)
+    response.end('Not Found.')
+
+  }
+})
+
+
+function showLeftTime()
+{
+	var now=new Date();
+	var year=now.getFullYear();
+	var month=now.getMonth() + 1;
+	var day=now.getDate();
+	var hours=now.getHours();
+	var minutes=now.getMinutes();
+	var seconds=now.getSeconds();
+    return year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds; 
+
+}
+
+deployServer.listen(PORT)
